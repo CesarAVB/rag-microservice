bdb4b27a50966c1a97ba439104d519636a8599de
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-02-01 15:27:16 -0300

    first commit

---
+++ src/main/java/br/com/cesaravb/ragmicroservice/RagMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesaravb.ragmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesaravb/ragmicroservice/RagMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesaravb.ragmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
